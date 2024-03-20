9009ebd1bd0855a97315a0056934cde50ad7533c
mohini mokati <phatakmohini@gmal.com>
2024-03-20 14:39:37 +0530

    first commit

--- aws_s3_bucket/main.tf
+++ aws_s3_bucket/main.tf
@@ -6,8 +6,13 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "some-shitty-bucket-name-qunitw"
-  # bucket    = "${var.environment}.my-bucket-yeeeeeeeeeee"
+# resource "aws_s3_bucket" "test" {
+#   bucket = "some-shitty-bucket-name-qunitw"
+#   # bucket    = "${var.environment}.my-bucket-yeeeeeeeeeee"
+# }
+
+resource "aws_instance" "myinstnace" {
+    ami = "ami-0f403e3180720dd7e"
+    instance_type = "t2.micro"
 }
 
